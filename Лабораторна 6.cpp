84a67bd2c2cf71e27643528ca9403d38981b5f8b
v250320-ui <v250320@gmail.com>
2025-12-12 04:43:06 +0200

    +
    +

--- Лабораторна 6.cpp
+++ Лабораторна 6.cpp
@@ -1,10 +1,8 @@
 ﻿#include <iostream>
 #include <iomanip>
-#include "windows.h"
 using namespace std;
-int main() {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+int main()
+{
 	double a, b, c, x, F;
 	cout << "print a: ";
 	cin >> a;

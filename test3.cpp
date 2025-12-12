95f9c0571000ff923d79ad66b67d6fdf7ad13692
v250320-ui <v250320@gmail.com>
2025-12-12 03:47:43 +0200

    Update test3.cpp
    перероблено код

--- test3.cpp
+++ test3.cpp
@@ -4,22 +4,28 @@
 using namespace std;
 
 int main() {
+
     float x;
+
     cout << "Enter x: ";
     cin >> x;
-    float sum = 0;
-    for (auto i = 1; i <= 5; i++) {
-        sum += i;
-    }
+
+    const float sum = 15.0f;
+
     float Y = log(sum) + x * x;
+
     cout << endl << "Y = " << Y << endl;
+
     cout << "Enter x for second formula: ";
     cin >> x;
-    float product = 1;
-    for (auto k = 1; k <= 5; k++) {
+
+    float product = 1.0f;
+    for (int k = 1; k <= 5; k++) {
         product *= tan(k * x);
     }
+
     float Z = product + sqrt(x);
+
     cout << "Z = " << Z << endl;
 
     return 0;
